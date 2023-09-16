2510f1d092ff8b59152dc55101801c4d35450748
sunnyswag <hhq126152@gmail.com>
2023-09-16 19:05:10 +0800

    finish imageClock

--- ImageClock/app/src/main/java/com/example/imageclock/MainActivity.kt
+++ ImageClock/app/src/main/java/com/example/imageclock/MainActivity.kt
@@ -4,26 +4,31 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Canvas
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.imageResource
-import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.imageclock.ui.theme.ImageClockTheme
 import java.util.Calendar
+import java.util.TimeZone
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,7 +40,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ImageClock()
                 }
             }
         }
@@ -43,75 +48,56 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Composable
 fun ImageClock() {
+    val calendar = remember { Calendar.getInstance() }
+    val currentTime = remember { mutableStateOf(System.currentTimeMillis()) }
+
+    val (hourTensResId, hourUnitsResId, minuteTensResId, minuteUnitsResId) =
+        rememberTimeResourceIds(calendar, currentTime)
+    val receiver = rememberBroadcastReceiver(calendar, currentTime)
+
     val context = LocalContext.current
-    val currentTime = remember { Calendar.getInstance() }
-    val currentHourTensResId by remember {
-        derivedStateOf { getResourceId(currentTime.get(Calendar.HOUR_OF_DAY) / 10) }
-    }
-    val currentHourUnitsResId by remember {
-        derivedStateOf { getResourceId(currentTime.get(Calendar.HOUR_OF_DAY) % 10) }
-    }
-    val currentMinuteTensResId by remember {
-        derivedStateOf { getResourceId(currentTime.get(Calendar.MINUTE) / 10) }
-    }
-    val currentMinuteUnitsResId by remember {
-        derivedStateOf { getResourceId(currentTime.get(Calendar.MINUTE) % 10) }
-    }
-
-
-    val intentReceiver = remember {
-        object: BroadcastReceiver() {
-            override fun onReceive(context: Context?, intent: Intent?) {
-                currentTime.timeInMillis = System.currentTimeMillis()
-            }
-        }
-    }
-
     DisposableEffect(Unit) {
         val filter = IntentFilter().apply {
             addAction(Intent.ACTION_TIME_TICK)
             addAction(Intent.ACTION_TIME_CHANGED)
             addAction(Intent.ACTION_TIMEZONE_CHANGED)
         }
-        context.registerReceiver(intentReceiver, filter)
+        context.registerReceiver(receiver, filter)
         onDispose {
-            context.unregisterReceiver(intentReceiver)
+            context.unregisterReceiver(receiver)
         }
     }
 
-    val hourTens = ImageBitmap.imageResource(id = currentHourTensResId)
-    val hourUnits = ImageBitmap.imageResource(id = currentHourUnitsResId)
-    val minuteTens = ImageBitmap.imageResource(id = currentMinuteTensResId)
-    val minuteUnits = ImageBitmap.imageResource(id = currentMinuteUnitsResId)
-
-    Canvas(modifier = Modifier) {
-        drawImage(
-            image = hourTens,
-            dstOffset = IntOffset(0, 0)
-        )
-        drawImage(
-            image = hourUnits,
-            dstOffset = IntOffset(100, 0)
-        )
-        drawImage(
-            image = minuteTens,
-            dstOffset = IntOffset(200, 0)
-        )
-        drawImage(
-            image = minuteUnits,
-            dstOffset = IntOffset(300, 0)
-        )
+    Column {
+        Row {
+            Image(painter = painterResource(hourTensResId), contentDescription = "hourTensImg")
+            Image(painter = painterResource(hourUnitsResId), contentDescription = "hourUnitsImg")
+        }
+        Row {
+            Image(painter = painterResource(minuteTensResId), contentDescription = "minuteTensImg")
+            Image(painter = painterResource(minuteUnitsResId), contentDescription = "minuteUnitsImg")
+        }
     }
+}
 
+@Composable
+private fun rememberBroadcastReceiver(
+    calendar: Calendar,
+    currentTime: MutableState<Long>
+) = remember {
+    object : BroadcastReceiver() {
+        @RequiresApi(Build.VERSION_CODES.R)
+        override fun onReceive(context: Context?, intent: Intent?) {
+            if (Intent.ACTION_TIMEZONE_CHANGED == intent?.action) {
+                val timeZone = TimeZone.getTimeZone(intent.getStringExtra(Intent.EXTRA_TIMEZONE))
+                calendar.timeZone = timeZone
+            }
+
+            currentTime.value = System.currentTimeMillis()
+            calendar.timeInMillis = currentTime.value
+        }
+    }
 }
 
 fun getResourceId(num: Int): Int {
@@ -127,5 +113,38 @@ fun getResourceId(num: Int): Int {
         8 -> R.drawable.clock_num_8
         9 -> R.drawable.clock_num_9
         else -> R.drawable.clock_num_0
+    }
+}
+
+data class TimeResourceIds(
+    val hourTensResId: Int,
+    val hourUnitsResId: Int,
+    val minuteTensResId: Int,
+    val minuteUnitsResId: Int
+)
+
+@Composable
+fun rememberTimeResourceIds(calendar: Calendar, currentTime: MutableState<Long>): TimeResourceIds {
+    val hourTensResId by remember(currentTime.value) {
+        derivedStateOf { getResourceId(calendar.get(Calendar.HOUR_OF_DAY) / 10) }
+    }
+    val hourUnitsResId by remember(currentTime.value) {
+        derivedStateOf { getResourceId(calendar.get(Calendar.HOUR_OF_DAY) % 10) }
+    }
+    val minuteTensResId by remember(currentTime.value) {
+        derivedStateOf { getResourceId(calendar.get(Calendar.MINUTE) / 10) }
+    }
+    val minuteUnitsResId by remember(currentTime.value) {
+        derivedStateOf { getResourceId(calendar.get(Calendar.MINUTE) % 10) }
+    }
+
+    return TimeResourceIds(hourTensResId, hourUnitsResId, minuteTensResId, minuteUnitsResId)
+}
+
+@Preview
+@Composable
+fun ImageClockPreview() {
+    ImageClockTheme {
+        ImageClock()
     }
 }
