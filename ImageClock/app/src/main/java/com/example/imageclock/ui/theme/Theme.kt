667ccceb8b35b9b8a98220e737510d2cd6152130
sunnyswag <hhq126152@gmail.com>
2023-09-17 15:12:03 +0800

    add systemuicontroller to implement immersive state

--- ImageClock/app/src/main/java/com/example/imageclock/ui/theme/Theme.kt
+++ ImageClock/app/src/main/java/com/example/imageclock/ui/theme/Theme.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -60,6 +61,13 @@ fun ImageClockTheme(
             window.statusBarColor = colorScheme.primary.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
+    }
+
+    val systemUiController = rememberSystemUiController()
+
+    SideEffect {
+        systemUiController.isNavigationBarVisible = false
+        systemUiController.isStatusBarVisible = false
     }
 
     MaterialTheme(
